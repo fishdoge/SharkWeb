1b38b9fea2e20364528265f6d3d09a2c8fdd5395
unknown <ru19wu06@gmail.com>
2022-12-16 02:02:13 +0800

    update title

--- src/routers/index.js
+++ src/routers/index.js
@@ -4,7 +4,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
   {
     path: '/',
-    name: 'Shark New Web',
+    name: 'MYMETA SHARK',
     component: () => import('@/views/HomePage.vue')
   },
   {
